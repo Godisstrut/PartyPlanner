1114d1f1ad713be277c549bc32f198c13a55c3fa
Godisstrut <erikh13255@gmail.com>
2026-03-02 13:57:21 +0100

    Home.tsx created
    Created home.tsx as a home page the user first visits, also added Tailwind

---
+++ party-project/src/Pages/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+    return(
+        <div className="flex flex-col items-center justify-center gap-4 h-screen bg-mist-900 text-white " >
+            <h4 className="font-bold text-4xl ">Välkommen</h4>
+            <h1 className="font-bold text-6xl text-amber-400 " >Du är inbjuden till Pauls 60års firande!</h1>
+        </div>
+    )
+}
+
+export default Home
