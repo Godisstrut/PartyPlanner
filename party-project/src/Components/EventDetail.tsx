51fabfb22d7aef6ed2889d19a988c4f1f0a0ca0a
Godisstrut <erikh13255@gmail.com>
2026-03-10 11:46:47 +0100

    EventsData
    Added a field for spots remaining in a given event

--- party-project/src/Components/EventDetail.tsx
+++ party-project/src/Components/EventDetail.tsx
@@ -10,14 +10,17 @@ function EventDetail() {
         return <p>Inget event hittades! Festen verkar inte finnas...</p>
     }
     return(
-        <div className="min-h-screen flex flex-col items-center pt-32" >
-            <h1>{event.title}</h1>
-            <p>{event.description}</p>
-            <p>{event.date}</p>
-            <p>{event.time}</p>
-            <p>{event.location}</p>
+        <div className="min-h-screen flex flex-col items-center pt-32 " >
+            <h1 className="text-4xl text-mauve-600 p-4 " >{event.title}</h1>
+            <p className="text-xl max-w-2xl text-center p-4" >{event.description}</p>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-12 p-6" >
+                <p className="text-lg" >{event.date}</p>
+                <p className="text-lg" >{event.time}</p>
+                <p className="text-lg" >{event.location}</p>
+                <p className="text-lg" >Antal platser kvar: {event.spots}</p>
+            </div>
             <div>
-                <h1>Boka din plats</h1>
+                <h1 className="text-2xl text-mauve-600" >Boka din plats</h1>
                 <form>
                     <input type="text" placeholder="Namn" className="border p-2 m-2 rounded" />
                     <input type="email" placeholder="E-post" className="border p-2 m-2 rounded" />
--- party-project/src/Data/EventsData.ts
+++ party-project/src/Data/EventsData.ts
@@ -5,6 +5,7 @@ export type Event = { // Type alias av hur en fest ska se ut
     time: string;
     location: string;
     description: string;
+    spots: number;
 }
 
 export const EventsData: Record<string, Event> = { // Fest data
@@ -14,7 +15,8 @@ export const EventsData: Record<string, Event> = { // Fest data
         date: "2026/10/3",
         time: "18:00",
         location: "Tockarpsvägen",
-        description: "Tjusig middagsbjuddning på t-100! Tre rätters middag med bubbel som fördrink kommer serveras, klä dig snyggt och kom som du är. "
+        description: "Tjusig middagsbjuddning på t-100! Tre rätters middag med bubbel som fördrink kommer serveras, klä dig snyggt och kom som du är. ",
+        spots: 20
     },
     molinvagenfest: {
         id: "molinvagenfest",
@@ -22,7 +24,8 @@ export const EventsData: Record<string, Event> = { // Fest data
         date: "2026/10/4",
         time: "19:00",
         location: "Molinvägen",
-        description: "Storslagen fest på molinvägen, klä dig i dina bästa partykläder och kom med ett glatt humör!"
+        description: "Storslagen fest på molinvägen, klä dig i dina bästa partykläder och kom med ett glatt humör!",
+        spots: 30
     },
     margretetorp: {
         id: "margretetorp",
@@ -30,7 +33,8 @@ export const EventsData: Record<string, Event> = { // Fest data
         date: "2026/09/30",
         time: "18:00",
         location: "Kägle väg 9, 266 98 Hjärnarp",
-        description: "Gammalgod fest på Margretetorps Gästgifvaregård! Festen inleds men fördrink och mingel på utomhus platsen, som sedan går vidare till middag och fest inne på deras största lokal. "
+        description: "Gammalgod fest på Margretetorps Gästgifvaregård! Festen inleds men fördrink och mingel på utomhus platsen, som sedan går vidare till middag och fest inne på deras största lokal. ",
+        spots: 50
     },
     molinvagenvin: {
         id: "molinvagenvin",
@@ -38,7 +42,8 @@ export const EventsData: Record<string, Event> = { // Fest data
         date: "2026/10/7",
         time: "17:00",
         location: "Molinvägen",
-        description: "Storslagen vinbjudning på molinvägen, klä dig i dina bästa partykläder och kom med ett glatt humör!"
+        description: "Storslagen vinbjudning på molinvägen, klä dig i dina bästa partykläder och kom med ett glatt humör!",
+        spots: 20
     },
     tockarpvin: {
         id: "tockarpvin",
@@ -46,7 +51,8 @@ export const EventsData: Record<string, Event> = { // Fest data
         date: "2026/10/8",
         time: "14:00",
         location: "Molinvägen",
-        description: "Storslagen vinbjudning på Tockarp, klä dig i dina bästa partykläder och kom med ett glatt humör!"
+        description: "Storslagen vinbjudning på Tockarp, klä dig i dina bästa partykläder och kom med ett glatt humör!",
+        spots: 25
     },
     margretetorpmiddag: {
         id: "margretetorpmiddag",
@@ -54,6 +60,7 @@ export const EventsData: Record<string, Event> = { // Fest data
         date: "2026/09/29",
         time: "18:00",
         location: "Kägle väg 9, 266 98 Hjärnarp",
-        description: "Gammalgod middag på Margretetorps Gästgifvaregård! Festen inleds men fördrink och mingel på utomhus platsen, som sedan går vidare till middag och fest inne på deras största lokal. "
+        description: "Gammalgod middag på Margretetorps Gästgifvaregård! Festen inleds men fördrink och mingel på utomhus platsen, som sedan går vidare till middag och fest inne på deras största lokal. ",
+        spots: 20
     }
 }
