b5aa128c8ef9e23565b54dd05325169c0f3a1f7b
Godisstrut <erikh13255@gmail.com>
2026-03-13 13:06:14 +0100

    Small admin update
    Added fields for the admindetails regarding events

--- party-project/src/Components/AdminEventDetails.tsx
+++ party-project/src/Components/AdminEventDetails.tsx
@@ -11,8 +11,13 @@ function AdminEventDetails() {
         return <p>Inget event hittades! Festen verkar inte finnas...</p>
     }
     return(
-        <div>
-            <h1> {event.title} </h1>
+        <div className="flex flex-col max-w-3xl mx-auto pt-24" >
+            <h1 className="text-3xl font-semibold text-mauve-600 p-4" > {event.title} </h1>
+            <p className="text-xl text-mauve-600 p-4" >Datum: {event.date} </p>
+            <p className="text-xl text-mauve-600 p-4" >Tid: {event.time} </p>
+            <p className="text-xl text-mauve-600 p-4" >Plats: {event.location} </p>
+            <p className="text-xl text-mauve-600 p-4" >Platser: {event.spots} </p>
+            <p className="text-xl font-semibold text-mauve-600 p-4" >Gäster: </p>
         </div>
     )
 }
--- party-project/src/Components/Footer.tsx
+++ party-project/src/Components/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
     return(
         <div>
-            <p className="text-xl text-mauve-600 text-center bg-mist-50" >Vid frågor kontakt oss på mail. henningsson.erik97@gmail.com</p>
+            <p className="text-xl text-mauve-600 text-center bg-mist-50" >Vid frågor kontakt oss på mail: henningsson.erik97@gmail.com</p>
         </div>
     )
 }
